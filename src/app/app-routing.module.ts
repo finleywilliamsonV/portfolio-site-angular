69368864be642a0efcf86ed9e03e0f3195433431
Finley Williamson V <finley.williamson@gmail.com>
2022-09-04 13:42:10 -0500

    Add basics for page-title, portfolio, and about-me page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,14 @@
 import { NgModule } from '@angular/core'
-import { RouterModule, Routes } from '@angular/router';
+import { RouterModule, Routes } from '@angular/router'
+import { AboutMeComponent } from './components/about-me/about-me.component'
+import { PortfolioComponent } from './components/portfolio/portfolio.component'
 import { WelcomePageComponent } from './components/welcome-page/welcome-page.component'
 
 const routes: Routes = [
     { path: '', redirectTo: '/home', pathMatch: 'full' },
     { path: 'home', component: WelcomePageComponent },
+    { path: 'portfolio', component: PortfolioComponent },
+    { path: 'about-me', component: AboutMeComponent },
 ]
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,12 +7,18 @@ import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { WelcomePageComponent } from './components/welcome-page/welcome-page.component';
 import { NavBarComponent } from './components/nav-bar/nav-bar.component';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { PortfolioComponent } from './components/portfolio/portfolio.component';
+import { AboutMeComponent } from './components/about-me/about-me.component';
+import { PageTitleComponent } from './components/page-title/page-title.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     WelcomePageComponent,
-    NavBarComponent
+    NavBarComponent,
+    PortfolioComponent,
+    AboutMeComponent,
+    PageTitleComponent
   ],
   imports: [
     BrowserModule,
