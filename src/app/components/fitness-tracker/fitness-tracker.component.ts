7fa36897da365149992734754fe6aa40b4645305
Finley Williamson V <finley.williamson@gmail.com>
2025-03-16 23:11:33 -0400

    Prog

--- src/app/components/fitness-tracker/fitness-tracker.component.ts
+++ src/app/components/fitness-tracker/fitness-tracker.component.ts
@@ -1,10 +1,101 @@
-import { Component } from '@angular/core'
-import { StopwatchComponent } from './stopwatch/stopwatch.component'
+import { TitleCasePipe } from '@angular/common'
+import { Component, computed, effect, signal } from '@angular/core'
+import { toObservable } from '@angular/core/rxjs-interop'
+import { FaIconComponent } from '@fortawesome/angular-fontawesome'
+import { faArrowRotateLeft, faInfo } from '@fortawesome/free-solid-svg-icons'
+import { interval, takeWhile } from 'rxjs'
+import { Exercise, MASTER_WORKOUT_DATA } from './fitness-tracker.model'
+
+enum TrackerState {
+  STARTED,
+  STOPPED
+}
+
+const getTransitionObject = (
+  transitionTo: string,
+  duration_sec: TransitionTime_sec
+): Exercise => ({
+  name: transitionTo,
+  duration_sec,
+  isTransition: true
+})
+
+const TRANSITION_TIMES_SEC = [0, 5, 10] as const
+
+type TransitionTime_sec = (typeof TRANSITION_TIMES_SEC)[number]
 
 @Component({
   selector: 'app-fitness-tracker',
-  imports: [StopwatchComponent],
+  imports: [FaIconComponent, TitleCasePipe],
   templateUrl: './fitness-tracker.component.html',
   styleUrl: './fitness-tracker.component.scss'
 })
-export class FitnessTrackerComponent {}
+export class FitnessTrackerComponent {
+  protected readonly TrackerState = TrackerState
+  protected readonly TransitionTimes = TRANSITION_TIMES_SEC
+  protected readonly workoutData = MASTER_WORKOUT_DATA
+  protected readonly infoIcon = faInfo
+  protected readonly refreshIcon = faArrowRotateLeft
+
+  protected readonly selectedDay = signal(0)
+  protected readonly selectedTransitionTime = signal<TransitionTime_sec>(0)
+
+  protected readonly currentWorkout = computed(() => {
+    const workout = this.workoutData[this.selectedDay()]
+    if (!workout) {
+      throw new Error('Index out of bounds in `currentWorkout`')
+    }
+    return workout
+  })
+
+  protected readonly currentExercises = computed<Exercise[]>(() =>
+    this.currentWorkout().exercises.reduce((agg, curr) => {
+      const _selectedTime = this.selectedTransitionTime()
+
+      if (_selectedTime > 0) {
+        agg.push(getTransitionObject(curr.name, _selectedTime))
+      }
+
+      agg.push(curr)
+      return agg
+    }, [] as Exercise[])
+  )
+
+  protected readonly currentRound = signal(1)
+  protected readonly currentExercise = computed<Exercise>(() => {
+    const exercise = this.currentWorkout()?.exercises[this.currentRound()]
+    if (!exercise) {
+      throw new Error('Index out of bounds in `currentExercise`')
+    }
+    return exercise
+  })
+  protected readonly nextExercise = computed(
+    () => this.currentWorkout().exercises[this.currentRound() + 1]
+  )
+
+  protected readonly currentState = signal(TrackerState.STOPPED)
+  protected readonly currentState$ = toObservable(this.currentState)
+
+  constructor() {
+    effect(() => {
+      console.log(this.currentExercises())
+    })
+  }
+
+  startCountdown = (): void => {
+    interval(1000)
+      .pipe(takeWhile(() => this.currentState() !== TrackerState.STOPPED))
+      .subscribe(() => {
+        this.currentExercise().duration_sec--
+      })
+  }
+
+  handleStartPress = (): void => {
+    this.currentState.set(TrackerState.STARTED)
+    this.startCountdown()
+  }
+
+  handleStopPress = (): void => {
+    this.currentState.set(TrackerState.STOPPED)
+  }
+}
--- src/app/components/fitness-tracker/fitness-tracker.model.ts
+++ src/app/components/fitness-tracker/fitness-tracker.model.ts
@@ -1,240 +1,326 @@
-export type Exercise =
-  | 'plank'
-  | 'air_squats'
-  | 'alternate_leg_lunges'
-  | 'knee_drives'
-  | 'down_ups'
+export type Exercise = {
+  name: string
+  duration_sec: number
+  isTransition: boolean
+}
 
-export interface Set {
-  exercises: { exercise: Exercise; durationSeconds: number }[]
-  rest_after_exercise_seconds: number
+export type WorkoutDay = {
+  day: number
+  isRestDay: boolean
+  exercises: Exercise[]
+  restAfterExercise_sec: number
   repetitions: number
 }
 
-export interface ContinuousRound {
-  exercises: { exercise: Exercise; durationSecs: number | 'reps' }[]
-  rounds: number | 'continuous'
-}
+export type WorkoutData = WorkoutDay[]
 
-export type Day =
-  | {
-      day: number
-      rest: true
-    }
-  | {
-      day: number
-      setInfo: Set
-    }
-
-export type WorkoutPlan = Day[]
-
-export const WORKOUT_PLAN: WorkoutPlan = [
+export const MASTER_WORKOUT_DATA: WorkoutData = [
   {
     day: 1,
-    setInfo: {
-      exercises: [
-        { exercise: 'plank', durationSeconds: 20 },
-        { exercise: 'air_squats', durationSeconds: 20 },
-        { exercise: 'alternate_leg_lunges', durationSeconds: 20 }
-      ],
-      rest_after_exercise_seconds: 60,
-      repetitions: 5
-    }
+    isRestDay: false,
+    exercises: [
+      {
+        name: 'Plank',
+        duration_sec: 20,
+        isTransition: false
+      },
+      {
+        name: 'Air Squats',
+        duration_sec: 20,
+        isTransition: false
+      },
+      {
+        name: 'Alternate Leg Lunges',
+        duration_sec: 20,
+        isTransition: false
+      }
+    ],
+    restAfterExercise_sec: 60,
+    repetitions: 5
   },
-  { day: 2, rest: true },
+  {
+    day: 2,
+    isRestDay: true,
+    exercises: [],
+    restAfterExercise_sec: 0,
+    repetitions: 0
+  },
   {
     day: 3,
-    setInfo: {
-      exercises: [
-        { exercise: 'knee_drives', durationSeconds: 30 },
-        { exercise: 'air_squats', durationSeconds: 30 },
-        { exercise: 'down_ups', durationSeconds: 30 }
-      ],
-      rest_after_exercise_seconds: 60,
-      repetitions: 5
-    }
+    isRestDay: false,
+    exercises: [
+      { name: 'Knee Drives', duration_sec: 30, isTransition: false },
+      { name: 'Air Squats', duration_sec: 30, isTransition: false },
+      { name: 'Down Ups', duration_sec: 30, isTransition: false }
+    ],
+    restAfterExercise_sec: 60,
+    repetitions: 5
   },
-  { day: 4, rest: true },
+  {
+    day: 4,
+    isRestDay: true,
+    exercises: [],
+    restAfterExercise_sec: 0,
+    repetitions: 0
+  },
   {
     day: 5,
-    setInfo: {
-      exercises: [
-        { exercise: 'knee_drives', durationSeconds: 20 },
-        { exercise: 'air_squats', durationSeconds: 20 },
-        { exercise: 'alternate_leg_lunges', durationSeconds: 45 }
-      ],
-      rest_after_exercise_seconds: 60,
-      repetitions: 6
-    }
+    isRestDay: false,
+    exercises: [
+      {
+        name: 'Knee Drives',
+        duration_sec: 20,
+        isTransition: false
+      },
+      {
+        name: 'Air Squats',
+        duration_sec: 20,
+        isTransition: false
+      },
+      { name: 'Alternate Leg Lunges', duration_sec: 45, isTransition: false }
+    ],
+    restAfterExercise_sec: 60,
+    repetitions: 6
   },
-  { day: 6, rest: true },
+  {
+    day: 6,
+    isRestDay: true,
+    exercises: [],
+    restAfterExercise_sec: 0,
+    repetitions: 0
+  },
   {
     day: 7,
-    setInfo: {
-      exercises: [
-        { exercise: 'plank', durationSeconds: 20 },
-        { exercise: 'air_squats', durationSeconds: 20 },
-        { exercise: 'alternate_leg_lunges', durationSeconds: 20 }
-      ],
-      rest_after_exercise_seconds: 60,
-      repetitions: 5
-    }
+    isRestDay: false,
+    exercises: [
+      {
+        name: 'Plank',
+        duration_sec: 20,
+        isTransition: false
+      },
+      {
+        name: 'Air Squats',
+        duration_sec: 20,
+        isTransition: false
+      },
+      {
+        name: 'Alternate Leg Lunges',
+        duration_sec: 20,
+        isTransition: false
+      }
+    ],
+    restAfterExercise_sec: 60,
+    repetitions: 5
   },
-  { day: 8, rest: true },
+  {
+    day: 8,
+    isRestDay: true,
+    exercises: [],
+    restAfterExercise_sec: 0,
+    repetitions: 0
+  },
   {
     day: 9,
-    setInfo: {
-      exercises: [
-        { exercise: 'knee_drives', durationSeconds: 30 },
-        { exercise: 'air_squats', durationSeconds: 30 },
-        { exercise: 'down_ups', durationSeconds: 30 }
-      ],
-      rest_after_exercise_seconds: 60,
-      repetitions: 5
-    }
+    isRestDay: false,
+    exercises: [
+      { name: 'Knee Drives', duration_sec: 30, isTransition: false },
+      { name: 'Air Squats', duration_sec: 30, isTransition: false },
+      { name: 'Down Ups', duration_sec: 30, isTransition: false }
+    ],
+    restAfterExercise_sec: 60,
+    repetitions: 5
   },
-  { day: 10, rest: true },
+  {
+    day: 10,
+    isRestDay: true,
+    exercises: [],
+    restAfterExercise_sec: 0,
+    repetitions: 0
+  },
   {
     day: 11,
-    setInfo: {
-      exercises: [
-        { exercise: 'plank', durationSeconds: 30 },
-        { exercise: 'air_squats', durationSeconds: 30 },
-        { exercise: 'alternate_leg_lunges', durationSeconds: 45 }
-      ],
-      rest_after_exercise_seconds: 60,
-      repetitions: 6
-    }
+    isRestDay: false,
+    exercises: [
+      { name: 'Plank', duration_sec: 30, isTransition: false },
+      { name: 'Air Squats', duration_sec: 30, isTransition: false },
+      { name: 'Alternate Leg Lunges', duration_sec: 45, isTransition: false }
+    ],
+    restAfterExercise_sec: 60,
+    repetitions: 6
   },
-  { day: 12, rest: true },
+  {
+    day: 12,
+    isRestDay: true,
+    exercises: [],
+    restAfterExercise_sec: 0,
+    repetitions: 0
+  },
   {
     day: 13,
-    setInfo: {
-      exercises: [
-        { exercise: 'knee_drives', durationSeconds: 30 },
-        { exercise: 'air_squats', durationSeconds: 30 },
-        { exercise: 'alternate_leg_lunges', durationSeconds: 30 }
-      ],
-      rest_after_exercise_seconds: 60,
-      repetitions: 6
-    }
+    isRestDay: false,
+    exercises: [
+      { name: 'Knee Drives', duration_sec: 30, isTransition: false },
+      { name: 'Air Squats', duration_sec: 30, isTransition: false },
+      { name: 'Alternate Leg Lunges', duration_sec: 30, isTransition: false }
+    ],
+    restAfterExercise_sec: 60,
+    repetitions: 6
   },
-  { day: 14, rest: true },
+  {
+    day: 14,
+    isRestDay: true,
+    exercises: [],
+    restAfterExercise_sec: 0,
+    repetitions: 0
+  },
   {
     day: 15,
-    setInfo: {
-      exercises: [
-        { exercise: 'knee_drives', durationSeconds: 30 },
-        { exercise: 'air_squats', durationSeconds: 30 },
-        { exercise: 'down_ups', durationSeconds: 30 }
-      ],
-      rest_after_exercise_seconds: 45,
-      repetitions: 6
-    }
+    isRestDay: false,
+    exercises: [
+      { name: 'Knee Drives', duration_sec: 30, isTransition: false },
+      { name: 'Air Squats', duration_sec: 30, isTransition: false },
+      { name: 'Down Ups', duration_sec: 30, isTransition: false }
+    ],
+    restAfterExercise_sec: 45,
+    repetitions: 6
   },
-  { day: 16, rest: true },
+  {
+    day: 16,
+    isRestDay: true,
+    exercises: [],
+    restAfterExercise_sec: 0,
+    repetitions: 0
+  },
   {
     day: 17,
-    setInfo: {
-      exercises: [
-        { exercise: 'plank', durationSeconds: 45 },
-        { exercise: 'air_squats', durationSeconds: 45 },
-        { exercise: 'knee_drives', durationSeconds: 45 }
-      ],
-      rest_after_exercise_seconds: 0,
-      repetitions: 3
-    }
+    isRestDay: false,
+    exercises: [
+      { name: 'Plank', duration_sec: 45, isTransition: false },
+      { name: 'Air Squats', duration_sec: 45, isTransition: false },
+      { name: 'Knee Drives', duration_sec: 45, isTransition: false }
+    ],
+    restAfterExercise_sec: 0,
+    repetitions: 3
   },
-  { day: 18, rest: true },
+  {
+    day: 18,
+    isRestDay: true,
+    exercises: [],
+    restAfterExercise_sec: 0,
+    repetitions: 0
+  },
   {
     day: 19,
-    setInfo: {
-      exercises: [
-        { exercise: 'air_squats', durationSeconds: 30 },
-        { exercise: 'alternate_leg_lunges', durationSeconds: 30 },
-        { exercise: 'down_ups', durationSeconds: 30 }
-      ],
-      rest_after_exercise_seconds: 60,
-      repetitions: 6
-    }
+    isRestDay: false,
+    exercises: [
+      { name: 'Air Squats', duration_sec: 30, isTransition: false },
+      { name: 'Alternate Leg Lunges', duration_sec: 30, isTransition: false },
+      { name: 'Down Ups', duration_sec: 30, isTransition: false }
+    ],
+    restAfterExercise_sec: 60,
+    repetitions: 6
   },
-  { day: 20, rest: true },
+  {
+    day: 20,
+    isRestDay: true,
+    exercises: [],
+    restAfterExercise_sec: 0,
+    repetitions: 0
+  },
   {
     day: 21,
-    setInfo: {
-      exercises: [
-        { exercise: 'plank', durationSeconds: 30 },
-        { exercise: 'air_squats', durationSeconds: 30 },
-        { exercise: 'alternate_leg_lunges', durationSeconds: 30 }
-      ],
-      rest_after_exercise_seconds: 60,
-      repetitions: 8
-    }
+    isRestDay: false,
+    exercises: [
+      { name: 'Plank', duration_sec: 30, isTransition: false },
+      { name: 'Air Squats', duration_sec: 30, isTransition: false },
+      { name: 'Alternate Leg Lunges', duration_sec: 30, isTransition: false }
+    ],
+    restAfterExercise_sec: 60,
+    repetitions: 8
   },
-  { day: 22, rest: true },
+  {
+    day: 22,
+    isRestDay: true,
+    exercises: [],
+    restAfterExercise_sec: 0,
+    repetitions: 0
+  },
   {
     day: 23,
-    setInfo: {
-      exercises: [
-        { exercise: 'knee_drives', durationSeconds: 30 },
-        { exercise: 'air_squats', durationSeconds: 30 },
-        { exercise: 'down_ups', durationSeconds: 30 }
-      ],
-      rest_after_exercise_seconds: 60,
-      repetitions: 8
-    }
+    isRestDay: false,
+    exercises: [
+      { name: 'Knee Drives', duration_sec: 30, isTransition: false },
+      { name: 'Air Squats', duration_sec: 30, isTransition: false },
+      { name: 'Down Ups', duration_sec: 30, isTransition: false }
+    ],
+    restAfterExercise_sec: 60,
+    repetitions: 8
   },
-  { day: 24, rest: true },
+  {
+    day: 24,
+    isRestDay: true,
+    exercises: [],
+    restAfterExercise_sec: 0,
+    repetitions: 0
+  },
   {
     day: 25,
-    setInfo: {
-      exercises: [
-        { exercise: 'plank', durationSeconds: 40 },
-        { exercise: 'air_squats', durationSeconds: 40 },
-        { exercise: 'knee_drives', durationSeconds: 40 },
-        { exercise: 'alternate_leg_lunges', durationSeconds: 40 }
-      ],
-      rest_after_exercise_seconds: 15,
-      repetitions: 3
-    }
+    isRestDay: false,
+    exercises: [
+      { name: 'Plank', duration_sec: 40, isTransition: false },
+      { name: 'Air Squats', duration_sec: 40, isTransition: false },
+      { name: 'Knee Drives', duration_sec: 40, isTransition: false },
+      { name: 'Alternate Leg Lunges', duration_sec: 40, isTransition: false }
+    ],
+    restAfterExercise_sec: 15,
+    repetitions: 3
   },
-  { day: 26, rest: true },
+  {
+    day: 26,
+    isRestDay: true,
+    exercises: [],
+    restAfterExercise_sec: 0,
+    repetitions: 0
+  },
   {
     day: 27,
-    setInfo: {
-      exercises: [
-        { exercise: 'air_squats', durationSeconds: 30 },
-        { exercise: 'alternate_leg_lunges', durationSeconds: 30 },
-        { exercise: 'down_ups', durationSeconds: 30 }
-      ],
-      rest_after_exercise_seconds: 60,
-      repetitions: 8
-    }
+    isRestDay: false,
+    exercises: [
+      { name: 'Air Squats', duration_sec: 30, isTransition: false },
+      { name: 'Alternate Leg Lunges', duration_sec: 30, isTransition: false },
+      { name: 'Down Ups', duration_sec: 30, isTransition: false }
+    ],
+    restAfterExercise_sec: 60,
+    repetitions: 8
   },
-  { day: 28, rest: true },
+  {
+    day: 28,
+    isRestDay: true,
+    exercises: [],
+    restAfterExercise_sec: 0,
+    repetitions: 0
+  },
   {
     day: 29,
-    setInfo: {
-      exercises: [
-        { exercise: 'plank', durationSeconds: 45 },
-        { exercise: 'air_squats', durationSeconds: 45 },
-        { exercise: 'knee_drives', durationSeconds: 45 },
-        { exercise: 'alternate_leg_lunges', durationSeconds: 45 }
-      ],
-      rest_after_exercise_seconds: 15,
-      repetitions: 3
-    }
+    isRestDay: false,
+    exercises: [
+      { name: 'Plank', duration_sec: 45, isTransition: false },
+      { name: 'Air Squats', duration_sec: 45, isTransition: false },
+      { name: 'Knee Drives', duration_sec: 45, isTransition: false },
+      { name: 'Alternate Leg Lunges', duration_sec: 45, isTransition: false }
+    ],
+    restAfterExercise_sec: 15,
+    repetitions: 3
   },
   {
     day: 30,
-    setInfo: {
-      exercises: [
-        { exercise: 'air_squats', durationSeconds: 60 },
-        { exercise: 'knee_drives', durationSeconds: 60 },
-        { exercise: 'down_ups', durationSeconds: 60 }
-      ],
-      rest_after_exercise_seconds: 0,
-      repetitions: 1
-    }
+    isRestDay: false,
+    exercises: [
+      { name: 'Air Squats', duration_sec: 60, isTransition: false },
+      { name: 'Knee Drives', duration_sec: 60, isTransition: false },
+      { name: 'Down Ups', duration_sec: 60, isTransition: false }
+    ],
+    restAfterExercise_sec: 0,
+    repetitions: 1
   }
 ]
